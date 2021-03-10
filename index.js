77b2f632e0126a005c0cf19ec69db06a1b6ad7fb
MobileAppProgramming <s21810001@student.unklab.ac.id>
2021-03-10 12:42:24 +0800

    menambahkan baris keempat menambahkan file home.js

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ console.log("Baris Pertama");
 console.log("Baris Kedua");
 console.log("Baris Ketiga");
 
+
+//Rabu
+console.log("Baris Keempat");
+
+//tanda titik di command git berarti untuk menambahkan semua file
